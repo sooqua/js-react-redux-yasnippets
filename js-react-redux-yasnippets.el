171c635780e5fed74faa1758bc3be1cce2774e6a
sooqua <sooqua751@gmail.com>
2019-08-24 08:56:42 +0300

    Change commentary

--- js-react-redux-yasnippets.el
+++ js-react-redux-yasnippets.el
@@ -23,8 +23,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;; ES7 Snippets
+;; JavaScript,React,Redux yasnippets
 
 ;;; Code:
 (require 'yasnippet)
